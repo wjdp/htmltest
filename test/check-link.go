f6432ffcfb4be476bac3938a1710905405ee7320
Will Pimblett <will@wjdp.uk>
2016-11-03 01:40:08 +0000

    Check for valid ports, mangled links &c
    GoSublime is reformatting files, will do a go fmt in a bit
    Fixed error in directory routine, incorrect parameter used

--- test/check-link.go
+++ test/check-link.go
@@ -1,257 +1,271 @@
 package test
 
 import (
-  "log"
-  // "fmt"
-  "os"
-  "path"
-  "strings"
-  "golang.org/x/net/html"
-  "net/url"
-  "net/http"
-  "github.com/wjdp/htmltest/issues"
-  "github.com/wjdp/htmltest/doc"
-  "github.com/wjdp/htmltest/refcache"
+	"log"
+	// "fmt"
+	"github.com/wjdp/htmltest/doc"
+	"github.com/wjdp/htmltest/issues"
+	"github.com/wjdp/htmltest/refcache"
+	"golang.org/x/net/html"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 )
 
 func CheckLink(document *doc.Document, node *html.Node) {
-  attrs := extractAttrs(node.Attr, []string{"href", "rel", "data-proofer-ignore"})
+	attrs := extractAttrs(node.Attr, []string{"href", "rel", "data-proofer-ignore"})
 
-  // Do not check canonical links
-  if attrs["rel"] == "canonical" { return }
-  // Ignore if data-proofer-ignore set
-  if attrPresent(node.Attr, "data-proofer-ignore") { return }
+	// Do not check canonical links
+	if attrs["rel"] == "canonical" {
+		return
+	}
+	// Ignore if data-proofer-ignore set
+	if attrPresent(node.Attr, "data-proofer-ignore") {
+		return
+	}
 
-  // Check href present
-  if !attrPresent(node.Attr, "href") {
-    issues.AddIssue(issues.Issue{
-      Level: issues.DEBUG,
-      Message: "anchor without href",
-      Document: document,
-    })
-    return
-  }
+	// Check href present
+	if !attrPresent(node.Attr, "href") {
+		issues.AddIssue(issues.Issue{
+			Level:    issues.DEBUG,
+			Message:  "anchor without href",
+			Document: document,
+		})
+		return
+	}
 
-  // Create reference
-  ref := doc.NewReference(document, node, attrs["href"])
+	// Create reference
+	ref := doc.NewReference(document, node, attrs["href"])
 
-  // Blank href
-  if attrs["href"] == "" {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "href blank",
-      Reference: ref,
-    })
-    return
-  }
+	// Blank href
+	if attrs["href"] == "" {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "href blank",
+			Reference: ref,
+		})
+		return
+	}
 
-  // href="#"
-  if attrs["href"] == "#" {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "empty hash",
-      Reference: ref,
-    })
-    return
-  }
+	// href="#"
+	if attrs["href"] == "#" {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "empty hash",
+			Reference: ref,
+		})
+		return
+	}
 
-  // Route link check
-  switch ref.Scheme {
-  case "http":
-    if Opts.EnforceHTTPS {
-      issues.AddIssue(issues.Issue{
-        Level: issues.ERROR,
-        Message: "is not an HTTPS link",
-        Reference: ref,
-        })
-    }
-    CheckExternal(ref)
-  case "https":
-    CheckExternal(ref)
-  case "file":
-    CheckInternal(ref)
-  case "mailto":
-    CheckMailto(ref)
-  case "tel":
-    CheckTel(ref)
-  }
+	// Route link check
+	switch ref.Scheme {
+	case "http":
+		if Opts.EnforceHTTPS {
+			issues.AddIssue(issues.Issue{
+				Level:     issues.ERROR,
+				Message:   "is not an HTTPS link",
+				Reference: ref,
+			})
+		}
+		CheckExternal(ref)
+	case "https":
+		CheckExternal(ref)
+	case "file":
+		CheckInternal(ref)
+	case "mailto":
+		CheckMailto(ref)
+	case "tel":
+		CheckTel(ref)
+	}
 }
 
 func CheckExternal(ref *doc.Reference) {
-  if !Opts.CheckExternal {
-    issues.AddIssue(issues.Issue{
-      Level: issues.DEBUG,
-      Message: "skipping",
-      Reference: ref,
-    })
-    return
-  }
+	if !Opts.CheckExternal {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.DEBUG,
+			Message:   "skipping",
+			Reference: ref,
+		})
+		return
+	}
 
-  urlStr := doc.URLString(ref)
-  if Opts.StripQueryString && !InList(Opts.StripQueryExcludes, urlStr) {
-    urlStr = doc.URLStripQueryString(urlStr)
-  }
-  var statusCode int
+	urlStr := doc.URLString(ref)
+	if Opts.StripQueryString && !InList(Opts.StripQueryExcludes, urlStr) {
+		urlStr = doc.URLStripQueryString(urlStr)
+	}
+	var statusCode int
 
-  if refcache.CachedURLStatus(urlStr) != 0 {
-    // If we have the result in cache, return that
-    statusCode = refcache.CachedURLStatus(urlStr)
-  } else {
-    // log.Println("Ext", ref.Document.Path, doc.URLString(ref))
-    urlUrl, err := url.Parse(urlStr)
-    req := &http.Request{
-      Method: "GET",
-      URL: urlUrl,
-      Header: map[string][]string{
-        "Range": {"bytes=0-63"}, // If server supports prevents body being sent
-      },
-    }
-    _ = req
-    resp, err := httpClient.Do(req)
-    // resp, err := httpClient.Get(urlStr)
+	if refcache.CachedURLStatus(urlStr) != 0 {
+		// If we have the result in cache, return that
+		statusCode = refcache.CachedURLStatus(urlStr)
+	} else {
+		// log.Println("Ext", ref.Document.Path, doc.URLString(ref))
+		urlUrl, err := url.Parse(urlStr)
+		req := &http.Request{
+			Method: "GET",
+			URL:    urlUrl,
+			Header: map[string][]string{
+				"Range": {"bytes=0-63"}, // If server supports prevents body being sent
+			},
+		}
+		_ = req
+		resp, err := httpClient.Do(req)
+		// resp, err := httpClient.Get(urlStr)
 
-    if err != nil {
-      if strings.Contains(err.Error(), "Client.Timeout exceeded while awaiting headers") {
-        issues.AddIssue(issues.Issue{
-          Level: issues.ERROR,
-          Message: "request timed out",
-          Reference: ref,
-        })
-        return
-      }
-      if strings.Contains(err.Error(), "no such host") {
-        issues.AddIssue(issues.Issue{
-          Level: issues.ERROR,
-          Message: "no such host",
-          Reference: ref,
-        })
-        return
-      }
-      if strings.Contains(err.Error(), "getsockopt: network is unreachable") {
-        issues.AddIssue(issues.Issue{
-          Level: issues.ERROR,
-          Message: "getsockopt: network is unreachable",
-          Reference: ref,
-        })
-      }
-      if strings.Contains(err.Error(), "write on closed buffer") {
-        issues.AddIssue(issues.Issue{
-          Level: issues.ERROR,
-          Message: err.Error(),
-          Reference: ref,
-        })
-        return
-      }
-      issues.AddIssue(issues.Issue{
-        Level: issues.ERROR,
-        Message: err.Error(),
-        Reference: ref,
-      })
-      log.Println("Unhandled httpClient error:", err.Error())
-    }
-    // Save cached result
-    refcache.SetCachedURLStatus(urlStr, resp.StatusCode)
-    statusCode = resp.StatusCode
-    // if statusCode == 200 { log.Println(urlStr) }
-  }
+		if err != nil {
+			if strings.Contains(err.Error(), "Client.Timeout exceeded while awaiting headers") {
+				issues.AddIssue(issues.Issue{
+					Level:     issues.ERROR,
+					Message:   "request timed out",
+					Reference: ref,
+				})
+				return
+			}
+			if strings.Contains(err.Error(), "no such host") {
+				issues.AddIssue(issues.Issue{
+					Level:     issues.ERROR,
+					Message:   "no such host",
+					Reference: ref,
+				})
+				return
+			}
+			if strings.Contains(err.Error(), "getsockopt: network is unreachable") {
+				issues.AddIssue(issues.Issue{
+					Level:     issues.ERROR,
+					Message:   "getsockopt: network is unreachable",
+					Reference: ref,
+				})
+			}
+			if strings.Contains(err.Error(), "write on closed buffer") {
+				issues.AddIssue(issues.Issue{
+					Level:     issues.ERROR,
+					Message:   err.Error(),
+					Reference: ref,
+				})
+				return
+			}
+			if strings.Contains(err.Error(), "unknown port") {
+				issues.AddIssue(issues.Issue{
+					Level:     issues.ERROR,
+					Message:   "unknown port",
+					Reference: ref,
+				})
+				return
+			}
+			// Unhandled client error, return generic error
+			issues.AddIssue(issues.Issue{
+				Level:     issues.ERROR,
+				Message:   err.Error(),
+				Reference: ref,
+			})
+			log.Println("Unhandled httpClient error:", err.Error())
+			return
+		}
+		// Save cached result
+		refcache.SetCachedURLStatus(urlStr, resp.StatusCode)
+		statusCode = resp.StatusCode
+		// if statusCode == 200 { log.Println(urlStr) }
+	}
 
-  switch statusCode {
-  case http.StatusOK://, http.StatusPartialContent:
-    issues.AddIssue(issues.Issue{
-      Level: issues.DEBUG,
-      Message: http.StatusText(statusCode),
-      Reference: ref,
-    })
-  case http.StatusPartialContent:
-    issues.AddIssue(issues.Issue{
-      Level: issues.INFO,
-      Message: http.StatusText(statusCode),
-      Reference: ref,
-    })
-  default:
-    // log.Println(urlStr)
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: http.StatusText(statusCode),
-      Reference: ref,
-    })
-  }
+	switch statusCode {
+	case http.StatusOK: //, http.StatusPartialContent:
+		issues.AddIssue(issues.Issue{
+			Level:     issues.DEBUG,
+			Message:   http.StatusText(statusCode),
+			Reference: ref,
+		})
+	case http.StatusPartialContent:
+		issues.AddIssue(issues.Issue{
+			Level:     issues.INFO,
+			Message:   http.StatusText(statusCode),
+			Reference: ref,
+		})
+	default:
+		// log.Println(urlStr)
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   http.StatusText(statusCode),
+			Reference: ref,
+		})
+	}
 
-  // TODO check a hash id exists in external page if present in reference (URL.Fragment)
+	// TODO check a hash id exists in external page if present in reference (URL.Fragment)
 
 }
 
 func CheckInternal(ref *doc.Reference) {
-  if !Opts.CheckInternal {
-    issues.AddIssue(issues.Issue{
-      Level: issues.DEBUG,
-      Message: "skipping",
-      Reference: ref,
-    })
-    return
-  }
-  // log.Println("CheckInternal", ref.Document.Path, doc.AbsolutePath(ref))
+	if !Opts.CheckInternal {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.DEBUG,
+			Message:   "skipping",
+			Reference: ref,
+		})
+		return
+	}
+	// log.Println("CheckInternal", ref.Document.Path, doc.AbsolutePath(ref))
 
-  CheckFile(ref, doc.AbsolutePath(ref))
+	CheckFile(ref, doc.AbsolutePath(ref))
 }
 
 func CheckFile(ref *doc.Reference, fPath string) {
-  // fPath should be relative to site root
-  checkPath := path.Join(Opts.DirectoryPath, fPath)
-  f, err := os.Stat(checkPath)
-  if os.IsNotExist(err) {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "target does not exist",
-      Reference: ref,
-    })
-    return
-  }
-  checkErr(err) // Crash on other errors
+	// fPath should be relative to site root
+	checkPath := path.Join(Opts.DirectoryPath, fPath)
+	f, err := os.Stat(checkPath)
+	if os.IsNotExist(err) {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "target does not exist",
+			Reference: ref,
+		})
+		return
+	}
+	checkErr(err) // Crash on other errors
 
-  if f.IsDir() {
-    issues.AddIssue(issues.Issue{
-      Level: issues.DEBUG,
-      Message: "target is a directory",
-      Reference: ref,
-    })
-    CheckFile(ref, path.Join(fPath, Opts.DirectoryIndex))
-    return
-  }
+	if f.IsDir() {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.DEBUG,
+			Message:   "target is a directory",
+			Reference: ref,
+		})
+		CheckFile(ref, path.Join(fPath, Opts.DirectoryIndex))
+		return
+	}
 }
 
 func CheckMailto(ref *doc.Reference) {
-  if !Opts.CheckMailto {
-    return
-  }
-  if len(ref.URL.Opaque) == 0 {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "mailto is empty",
-      Reference: ref,
-    })
-    return
-  }
-  if !strings.Contains(ref.URL.Opaque, "@") {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "contains an invalid email address",
-      Reference: ref,
-    })
-    return
-  }
+	if !Opts.CheckMailto {
+		return
+	}
+	if len(ref.URL.Opaque) == 0 {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "mailto is empty",
+			Reference: ref,
+		})
+		return
+	}
+	if !strings.Contains(ref.URL.Opaque, "@") {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "contains an invalid email address",
+			Reference: ref,
+		})
+		return
+	}
 }
 
 func CheckTel(ref *doc.Reference) {
-  if !Opts.CheckTel {
-    return
-  }
-  if len(ref.URL.Opaque) == 0 {
-    issues.AddIssue(issues.Issue{
-      Level: issues.ERROR,
-      Message: "tel is empty",
-      Reference: ref,
-    })
-    return
-  }
+	if !Opts.CheckTel {
+		return
+	}
+	if len(ref.URL.Opaque) == 0 {
+		issues.AddIssue(issues.Issue{
+			Level:     issues.ERROR,
+			Message:   "tel is empty",
+			Reference: ref,
+		})
+		return
+	}
 }
--- test/htmltest.go
+++ test/htmltest.go
@@ -1,162 +1,161 @@
 package test
 
 import (
-  "log"
-  "os"
-  "time"
-  "path"
-  "sync"
-  "golang.org/x/net/html"
-  "net/http"
-  "github.com/wjdp/htmltest/issues"
-  "github.com/wjdp/htmltest/doc"
+	"github.com/wjdp/htmltest/doc"
+	"github.com/wjdp/htmltest/issues"
+	"golang.org/x/net/html"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"sync"
+	"time"
 )
-
 
 var httpClient *http.Client
 
 func setup() {
-  transport := &http.Transport{
-    TLSNextProto: nil, // Disable HTTP/2, "write on closed buffer" errors
-  }
-  httpClient = &http.Client{
-    // Durations are in nanoseconds
-    Transport: transport,
-    Timeout: time.Duration(Opts.ExternalTimeout * 1000000000),
-  }
+	transport := &http.Transport{
+		TLSNextProto: nil, // Disable HTTP/2, "write on closed buffer" errors
+	}
+	httpClient = &http.Client{
+		// Durations are in nanoseconds
+		Transport: transport,
+		Timeout:   time.Duration(Opts.ExternalTimeout * 1000000000),
+	}
 }
 
 func Test(optsUser map[string]interface{}) {
-  SetOptions(optsUser)
-  setup() // Setup objects requiring options
-  issues.InitIssueStore()
+	SetOptions(optsUser)
+	setup() // Setup objects requiring options
+	issues.InitIssueStore()
 
-  if Opts.FilePath != "" {
-    doc := doc.Document{
-      // Directory: Opts.DirectoryPath,
-      Path: Opts.FilePath,
-    }
-    TestFile(&doc)
-  } else if Opts.DirectoryPath != "" {
-    TestDirectory(Opts)
-  } else {
-    log.Fatal("Neither file or directory path provided")
-  }
+	if Opts.FilePath != "" {
+		doc := doc.Document{
+			// Directory: Opts.DirectoryPath,
+			Path: Opts.FilePath,
+		}
+		TestFile(&doc)
+	} else if Opts.DirectoryPath != "" {
+		TestDirectory(Opts)
+	} else {
+		log.Fatal("Neither file or directory path provided")
+	}
 }
 
 func makePath(p string) string {
-  return path.Join(Opts.DirectoryPath, p)
+	return path.Join(Opts.DirectoryPath, p)
 }
 
 func TestDirectory(opts Options) {
-  issues.LogLevel = Opts.LogLevel
+	issues.LogLevel = Opts.LogLevel
 
-  log.Printf("htmltest started on %s", Opts.DirectoryPath)
+	log.Printf("htmltest started on %s", Opts.DirectoryPath)
 
-  files := RecurseDirectory("")
-  TestFiles(files)
-  // issues.OutputIssues()
+	files := RecurseDirectory("")
+	TestFiles(files)
+	// issues.OutputIssues()
 
-  log.Printf("%d files checked", len(files))
+	log.Printf("%d files checked", len(files))
 }
 
 // Walk through the directory tree and pick .html files
 func RecurseDirectory(dPath string) []doc.Document {
-  documents := make([]doc.Document, 0)
+	documents := make([]doc.Document, 0)
 
-  // Open dPath
-  f, err := os.Open( makePath(Opts.DirectoryPath) )
-  checkErr(err)
-  defer f.Close()
+	// Open dPath
+	f, err := os.Open(makePath(dPath))
+	checkErr(err)
+	defer f.Close()
 
-  // Get FileInfo of dPath
-  fi, err := f.Stat()
-  checkErr(err)
+	// Get FileInfo of dPath
+	fi, err := f.Stat()
+	checkErr(err)
 
-  if fi.IsDir() {
-    // Read all FileInfo-s from dPath
-    fis, err := f.Readdir(-1)
-    checkErr(err)
+	if fi.IsDir() {
+		// Read all FileInfo-s from dPath
+		fis, err := f.Readdir(-1)
+		checkErr(err)
 
-    // Iterate over contents of dPath
-    for _, fileinfo := range fis {
-      fPath := path.Join(dPath, fileinfo.Name())
-      if fileinfo.IsDir() {
-        // If item is a dir, we need to iterate further, save returned documents
-        documents = append(documents, RecurseDirectory(fPath)...)
-      } else if path.Ext(fileinfo.Name()) == ".html" {
-        // If a file, save to filename list
-        documents = append(documents, doc.Document{
-          Directory: dPath,
-          Path: fPath,
-        })
-      }
-    }
-  } else {
-    log.Fatalf("%s isn't a directory", dPath)
-  }
+		// Iterate over contents of dPath
+		for _, fileinfo := range fis {
+			fPath := path.Join(dPath, fileinfo.Name())
+			if fileinfo.IsDir() {
+				// If item is a dir, we need to iterate further, save returned documents
+				documents = append(documents, RecurseDirectory(fPath)...)
+			} else if path.Ext(fileinfo.Name()) == ".html" {
+				// If a file, save to filename list
+				documents = append(documents, doc.Document{
+					Directory: dPath,
+					Path:      fPath,
+				})
+			}
+		}
+	} else {
+		log.Fatalf("%s isn't a directory", dPath)
+	}
 
-  return documents
+	return documents
 }
 
 func checkErr(err error) {
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func TestFiles(documents []doc.Document) {
 
-  if Opts.TestFilesConcurrently {
-    var wg sync.WaitGroup
-    for _, document := range documents {
-      wg.Add(1)
-      go func(document doc.Document) {
-        defer wg.Done()
-        TestFile(&document)
-      }(document)
-    }
-    wg.Wait()
-  } else {
-    for _, document := range documents {
-      TestFile(&document)
-    }
-  }
+	if Opts.TestFilesConcurrently {
+		var wg sync.WaitGroup
+		for _, document := range documents {
+			wg.Add(1)
+			go func(document doc.Document) {
+				defer wg.Done()
+				TestFile(&document)
+			}(document)
+		}
+		wg.Wait()
+	} else {
+		for _, document := range documents {
+			TestFile(&document)
+		}
+	}
 }
 
 func TestFile(document *doc.Document) {
-  // log.Println("testFile", document.Path)
-  f, err := os.Open( makePath(document.Path) )
-  checkErr(err)
-  defer f.Close()
+	// log.Println("testFile", document.Path)
+	f, err := os.Open(makePath(document.Path))
+	checkErr(err)
+	defer f.Close()
 
-  document.File = f
+	document.File = f
 
-  parseHtml(document)
+	parseHtml(document)
 }
 
 func parseHtml(document *doc.Document) {
-  doc, err := html.Parse(document.File)
-  checkErr(err)
-  document.HTMLNode = doc
-  parseNode(document, document.HTMLNode)
+	doc, err := html.Parse(document.File)
+	checkErr(err)
+	document.HTMLNode = doc
+	parseNode(document, document.HTMLNode)
 }
 
 func parseNode(document *doc.Document, n *html.Node) {
-  if n.Type == html.ElementNode {
-    switch n.Data {
-    case "a":
-      CheckLink(document, n)
-    case "img":
-      CheckImg(n)
-    case "link":
-      CheckLink(document, n)
-    case "script":
-      CheckScript(n)
-    }
-  }
-  // Iterate over children
-  for c := n.FirstChild; c != nil; c = c.NextSibling {
-    parseNode(document, c)
-  }
+	if n.Type == html.ElementNode {
+		switch n.Data {
+		case "a":
+			CheckLink(document, n)
+		case "img":
+			CheckImg(n)
+		case "link":
+			CheckLink(document, n)
+		case "script":
+			CheckScript(n)
+		}
+	}
+	// Iterate over children
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		parseNode(document, c)
+	}
 }
