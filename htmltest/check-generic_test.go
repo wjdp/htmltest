0a815de9e9b9d3a72a0a8798ee96da3c0ec6950b
Will Pimblett <will@wjdp.uk>
2016-11-15 20:18:38 +0000

    Add testing for many tags using checkGeneric.
    If these tags have their URL attributes these are checked for not
    being blank, and being valid. They are ignored if the attr is
    missing.
    
    List of tags and attributes checked follows:
    
    area: href
    blockquote: cite
    del: cite
    ins: cite
    q: cite
    iframe: src
    input: src
    audio: src
    embed: src
    source: src
    track: src
    video: src and poster

---
+++ htmltest/check-generic_test.go
@@ -0,0 +1,56 @@
+package htmltest
+
+import (
+	"github.com/wjdp/htmltest/issues"
+	"path"
+	"testing"
+)
+
+var genericTests = []struct {
+	fixture    string
+	errorCount int
+}{
+	{"areaValid.html", 0},
+	{"areaBroken.html", 1},
+	{"areaBlank.html", 2},
+	{"areaMissing.html", 0},
+	{"audioValid.html", 0},
+	{"audioBroken.html", 5},
+	{"audioBlank.html", 5},
+	{"audioMissing.html", 0},
+	{"citeValid.html", 0},
+	{"citeBroken.html", 4},
+	{"citeBlank.html", 4},
+	{"citeMissing.html", 0},
+	{"embedValid.html", 0},
+	{"embedBroken.html", 1},
+	{"embedBlank.html", 2},
+	{"embedMissing.html", 0},
+	{"iframeValid.html", 0},
+	{"iframeBroken.html", 1},
+	{"iframeBlank.html", 2},
+	{"iframeMissing.html", 0},
+	{"inputSrcValid.html", 0},
+	{"inputSrcBroken.html", 1},
+	{"inputSrcBlank.html", 2},
+	{"inputSrcMissing.html", 0},
+	{"objectValid.html", 0},
+	{"objectBroken.html", 2},
+	{"objectBlank.html", 2},
+	{"objectMissing.html", 0},
+	{"videoValid.html", 0},
+	{"videoBroken.html", 9},
+	{"videoBlank.html", 9},
+	{"videoMissing.html", 0},
+}
+
+func TestCheckGenericTable(t *testing.T) {
+	for _, gt := range genericTests {
+		hT := tTestFile(path.Join("fixtures/generic", gt.fixture))
+		c := hT.issueStore.Count(issues.LevelError)
+		if c != gt.errorCount {
+			t.Error("error count", c, "!=", gt.errorCount, "in", gt.fixture)
+			hT.issueStore.DumpIssues(true)
+		}
+	}
+}
