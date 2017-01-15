ce7861b52077d8b9cee09ea4e46916d188c34e65
Will Pimblett <will@wjdp.uk>
2017-01-15 18:45:20 +0000

    Release 0.3.1

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-const cmdVersion string = "dev"
+const cmdVersion string = "0.3.1"
 const cmdSeparator string = "========================================================================"
 
 var (
