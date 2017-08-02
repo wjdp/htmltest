3c231e4ee11f238c09533c97f3023076a49cdf78
Will Pimblett <will@wjdp.uk>
2017-08-02 22:34:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-go build -ldflags "-X main.buildDate=`date -u +%Y-%m-%dT%H:%M:%SZ`" -o bin/htmltest -x main.go
+# Script for devs to build a copy of the app with build flags set
+
+go build -ldflags "-X main.buildDate=`date -u +%Y-%m-%dT%H:%M:%SZ` -X main.version=`git describe --tags`" -o bin/htmltest -x main.go
