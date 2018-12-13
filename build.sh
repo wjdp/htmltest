cb4faddf03497ebf86b65ea4d16343587f79b933
Packager <packager@grml>
2018-12-13 14:41:37 +0200

    Revert build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Script for devs to build a copy of the app with build flags set
-export GOPATH=/home/packager/go
 
 go build -ldflags "-X main.date=`date -u +%Y-%m-%dT%H:%M:%SZ` -X main.version=`git describe --tags`" -o bin/htmltest -x main.go
